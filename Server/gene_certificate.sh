f7cc67c3d9e615084b3e10d39cee1309606811f4
sukhada369 <sukhada369@gmail.com>
2016-05-19 14:16:32 +0530

    unable to create certificate

--- Server/gene_certificate.sh
+++ Server/gene_certificate.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-
-
 sudo mkdir -p /etc/pki/tls/certs
-
-sudo mkdir /etc/pki/tls/private
-
+sudo mkdir -p /etc/pki/tls/private
 rm -rf /etc/ssl/openssl.cnf
-
 cp openssl.cnf /etc/ssl/openssl.cnf
-
 cd /etc/pki/tls
-
 sudo openssl req -config /etc/ssl/openssl.cnf -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout private/logstash-forwarder.key -out certs/logstash-forwarder.crt
