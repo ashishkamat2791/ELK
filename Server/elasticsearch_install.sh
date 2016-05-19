919e280388d29ba74b79a14ddb8486a63a8e12fc
sukhada369 <sukhada369@gmail.com>
2016-05-19 13:24:28 +0530

    Non interactive java8

--- Server/elasticsearch_install.sh
+++ Server/elasticsearch_install.sh
@@ -8,7 +8,7 @@ sudo add-apt-repository -y ppa:webupd8team/java
 
 sudo apt-get update
 
-sudo apt-get DEBIAN_FRONTEND=noninteractive -y --force-yes install oracle-java8-installer
+sudo apt-get -y --force-yes install oracle-java8-installer
 
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 
